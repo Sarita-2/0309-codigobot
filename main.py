709d3b1dce5341ddbb0c539b707bab525603e5c4
Sarita-2 <smunozmm1@gmail.com>
2024-09-03 20:35:40 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from bot_logic import gen_pass
+from discord.ext import commands 
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+bot = commands.Bot(command_prefix= "!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hi!")
+
+@bot.command()
+async def bye(ctx):
+    await ctx.send("😞")
+
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run("mi token")
